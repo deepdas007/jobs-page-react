1a619655dfff8cec2381c41ff6e7d9d1cd063ec1
deepdas007 <deepd1007@gmail.com>
2024-06-11 18:20:55 +0530

    Added Delete functionality and single job page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import AddJobPage from "./pages/AddJobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const App = () => {
     //Add Job API
@@ -24,11 +25,25 @@ const App = () => {
         });
     };
 
+    //Delete Job API
+    const deleteJob = async (id) => {
+        const res = await fetch(`/api/jobs/${id}`, {
+            method: "DELETE",
+        });
+
+        return;
+    };
+
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<HomePage />} />
                 <Route path="/jobs" element={<JobsPage />} />
+                <Route
+                    path="/job/:id"
+                    element={<JobPage deleteSelectedJob={deleteJob} />}
+                    loader={jobLoader}
+                />
                 <Route
                     path="/add-job"
                     element={<AddJobPage addJobSubmit={addJob} />}
